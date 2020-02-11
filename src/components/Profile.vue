fa28b3064365c3f73b39426ce7fee2f7f541ae78
GedeonV <zinni.arthur@gmail.com>
2020-02-11 15:39:47 +0100

    42

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -3,7 +3,7 @@
 		<v-gravatar :email="currentUser.email"/>
 		<h1 class="title">{{currentUser.nickname}}</h1>
 		<h2 class="subtitle">{{currentUser.last_name}} {{currentUser.first_name}}</h2>
-		<h3 class="title is-3">{{currentUser.age}}</h3>
+		<h3 class="title is-4">{{currentUser.age}}</h3>
 		<h2 class="subtitle">Email : {{currentUser.email}}</h2>
 		<h2 class="subtitle">Créer le : {{currentUser.created_at}}</h2>
 		
